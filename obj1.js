0f6834885ac5e20db5df24347bd9da49589d655d
777dan <danya.nikitin7@gmail.com>
2021-12-18 10:25:32 +0200

    first commit

---
+++ obj1.js
@@ -0,0 +1,43 @@
+function Dog(name, breed, weight) {
+    this.name = name;
+    this.breed = breed;
+    this.weight = weight;
+    this.speed = 0;
+    this.bark = function () {
+        if (weight < 20) {
+            alert(this.name + " сказал Тяв!");
+        }
+        else if (weight >= 20) {
+            alert(this.name + " сказал Гав!");
+        }
+        else {
+            alert("Error!")
+        }
+    };
+    this.start = function () {
+        this.speed = 1;
+    };
+    this.stop = function () {
+        this.speed = 0;
+    }
+    this.changeSpeed = function (amount) {
+        amount *= 2;
+    };
+    this.showSpeed = function () {
+        alert(this.speed);
+    };
+}
+
+let fido = new Dog("Fido", "Mixed", 38);
+let fluffy = new Dog("Fluffy", "Poodel", 38);
+let spot = new Dog("Spot", "Chihuahua", 10);
+
+let dogs = [fido, fluffy, spot];
+
+for (let i = 0; i < dogs.length; i++) {
+    alert(dogs[i].name);
+    dogs[i].bark();
+    dogs[i].start();
+    dogs[i].changeSpeed(2);
+    dogs[i].showSpeed();
+}
